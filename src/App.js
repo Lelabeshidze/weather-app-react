afa4b6dfe11027b6cb4eceede6bf1e7da8ff3965
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2022-11-15 22:25:58 +0400

    added styles

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
       })
       .catch((err) => console.log(err));
   };
-  console.log(currentWeather);
+  
   return (
     <div className="Container">
       <Search onSearchChange={handleOnSearchChange} />
