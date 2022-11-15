2406c189dff6ff912de7ad68d00de1827149c88f
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2022-11-15 22:33:42 +0400

    .

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "7e1d6d740emsh8afb3434e88904dp167b07jsne3206f9a077c",
+    "X-RapidAPI-Key":  "",// enter your rapid api key here,
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
@@ -9,4 +9,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY =""; // enter your key from openweather API
