e3afd27221f8a0643b2fca643eae9e217b7449cc
Lelabeshidze <94396022+Lelabeshidze@users.noreply.github.com>
2022-11-15 18:13:40 +0400

    fetch data from api

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "7e1d6d740emsh8afb3434e88904dp167b07jsne3206f9a077c",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
